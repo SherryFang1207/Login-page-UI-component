66e1bb30e09e29c63362d7591cd0b40705048b18
Ziqi Fang <zfang1207@gmail.com>
2023-07-09 12:31:48 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    fontFamily:{
+      'sans': ['Mulish', 'sans-serif'],
+      'mono': ['Rokkitt', 'serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
